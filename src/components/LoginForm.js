77b349b1f23c09def2f562c40b3c9b0328370388
Ken Kotch <hello@ken.uno>
2017-12-27 22:27:01 -0500

    add working firebase auth via email and password and add loading screen

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react'
-import { Button, Header, Card, CardSection, Input } from './common'
+import { Text } from 'react-native'
+import { Button, Header, Card, CardSection, Input, Spinner } from './common'
+import firebase from 'firebase'
 
 class LoginForm extends Component {
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: '',
+    loading: false
   }
 
-  componentWillMount() {
+  onButtonPress() {
+    const { email, password } = this.state
+    this.setState({ error: '', loading: true })
 
+    // attempt to login. if success, call onLoginSuccess, else attempt to create user, else call onLoginFail
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this))
+      })
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed.', loading: false })
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    })
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />
+    }
+    return (
+      <Button onPress={ this.onButtonPress.bind(this) }>
+        Login
+      </Button>
+    )
   }
 
   render() {
@@ -33,13 +71,23 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={ styles.errorTextStyle }>
+          { this.state.error }
+        </Text>
+
         <CardSection>
-          <Button onPress={ this.onButtonPress }>
-            Login
-          </Button>
+          { this.renderButton() }
         </CardSection>
       </Card>
     )
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
