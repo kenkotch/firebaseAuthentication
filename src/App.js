ac34739bd42574baad0be9ff025c4df7c990c707
Ken Kotch <hello@ken.uno>
2017-12-27 23:03:54 -0500

    auth complete. works as boilerplate firebase auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
-import { Header } from './components/common'
+import { Header, Button, CardSection, Spinner } from './components/common'
 import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  state = { loggedIn: null }
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyD6mFmQJzqHoUfRyy2sSQXZfWwPicNI-70',
@@ -14,6 +16,31 @@ class App extends Component {
       storageBucket: 'authentication-caa5c.appspot.com',
       messagingSenderId: '961244912211'
     })
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true })
+      } else {
+        this.setState({ loggedIn: false })
+      }
+    })
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut() }>
+              Log Out
+            </Button>
+          </CardSection>
+        )
+      case false:
+        return <LoginForm />
+      default:
+        return <Spinner />
+    }
   }
 
   render() {
@@ -21,7 +48,7 @@ class App extends Component {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        { this.renderContent() }
       </View>
     )
   }
